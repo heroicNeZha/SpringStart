a60342ab33740a15a0381c07bbdc381a99f3a8cc
lizewei <lizewei@mail.ustc.edu.cn>
2019-07-30 09:49:01 +0800

    复杂属性的注入

--- src/main/java/edu/ustc/SpringStart/dao/UserDao.java
+++ src/main/java/edu/ustc/SpringStart/dao/UserDao.java
@@ -1,7 +1,36 @@
 package edu.ustc.SpringStart.dao;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
 public class UserDao {
+    private String[] arrs;
+    private List<String> list;
+    private Map<String, String> map;
+    private Properties properties;
+
+    public void setArrs(String[] arrs) {
+        this.arrs = arrs;
+    }
+
+    public void setList(List<String> list) {
+        this.list = list;
+    }
+
+    public void setMap(Map<String, String> map) {
+        this.map = map;
+    }
+
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+    }
+
     public void call() {
         System.out.println("dao...");
+        System.out.println(arrs.toString());
+        System.out.println(list);
+        System.out.println(map);
+        System.out.println(properties);
     }
 }
