e38da3b97ad55d3ecfbff5275dc5d79296193792
lizewei <lizewei@mail.ustc.edu.cn>
2019-08-21 22:14:58 +0800

    Mybatis 调用存储过程

--- src/main/java/edu/ustc/SpringStart/person/mapper/PersonMapper.java
+++ src/main/java/edu/ustc/SpringStart/person/mapper/PersonMapper.java
@@ -3,11 +3,14 @@ package edu.ustc.SpringStart.person.mapper;
 import edu.ustc.SpringStart.person.POJO.Person;
 
 import java.util.List;
+import java.util.Map;
 
 public interface PersonMapper {
     List<Person> queryPersonByName(String name);
 
     Person queryPersonById(int id);
+
+    List<Person> queryPersonByAge(Map<String,Object> params);
 
     void addPerson(Person person);
 
--- src/test/java/TestMyBatis.java
+++ src/test/java/TestMyBatis.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TestMyBatis {
     @Test
@@ -14,15 +16,20 @@ public class TestMyBatis {
         Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
         SqlSession sqlSession = sqlSessionFactory.openSession();
-        Person person = new Person();
-        person.setName("wyl");
-        person.setAge(18);
-        person.setSex(false);
+//        Person person = new Person();
+//        person.setName("wyl");
+//        person.setAge(18);
+//        person.setSex(false);
         PersonMapper mapper = sqlSession.getMapper(PersonMapper.class);
-        mapper.addPerson(person);
-        System.out.println(mapper.queryAll());
-        sqlSession.commit();
-        System.out.println(mapper.queryAll());
+        Map<String,Object> map = new HashMap<>();
+        map.put("start_age",1);
+        map.put("end_age",20);
+        mapper.queryPersonByAge(map);
+        System.out.println(map.get("p_count"));
+//        mapper.addPerson(person);
+//        System.out.println(mapper.queryAll());
+//        sqlSession.commit();
+//        System.out.println(mapper.queryAll());
         sqlSession.close();
     }
 }
